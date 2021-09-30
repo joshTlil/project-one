1ad007c2e73c13d08888ae0fe32abb89aeb0266d
DCKII <dckornbluthii@bellsouth.net>
2021-09-30 12:36:23 -0400

    updated index demo javascript

---
+++ assets/js/index_demo.js
@@ -0,0 +1,8 @@
+var hideBtn = document.querySelector('#hide-btn');
+
+$(".show-btn").click(function() {
+    if ()
+    $("div").removeClass("hide");
+});
+
+// ("drop", {direction: "down"}, "slow");
