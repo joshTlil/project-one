b385480e63d7fb77c1571cbbd00c16ab4365d3a1
DCKII <dckornbluthii@bellsouth.net>
2021-09-30 13:50:48 -0400

    added buttons for each section of code demo

--- assets/js/index_demo.js
+++ assets/js/index_demo.js
@@ -1,13 +1,48 @@
-var showBtn = document.querySelector('#hide-btn');
-var clicked = true;
+var clicked1 = true;
+var clicked2 = true;
+var clicked3 = true;
+var clicked4 = true;
 
-showBtn.forEach.$('.show-btn').onclick(function() {
-    if (clicked) {
-        $('.switcher').hide();
-    } else if (!clicked) {
-        $('div').hide();
+$('.show-btn1').click(function() {
+    if (clicked1) {
+        $('.hide1').show();
+        clicked1 = false;
+    } else if (!clicked1) {
+        $('.hide1').hide();
+        clicked1 = true;
     }
     return
 });
 
-// 
+$('.show-btn2').click(function() {
+    if (clicked2) {
+        $('.hide2').show();
+        clicked2 = false;
+    } else if (!clicked2) {
+        $('.hide2').hide();
+        clicked2 = true;
+    }
+    return
+});
+
+$('.show-btn3').click(function() {
+    if (clicked3) {
+        $('.hide3').show();
+        clicked3 = false;
+    } else if (!clicked3) {
+        $('.hide3').hide();
+        clicked3 = true;
+    }
+    return
+});
+
+$('.show-btn4').click(function() {
+    if (clicked4) {
+        $('.hide4').show();
+        clicked4 = false;
+    } else if (!clicked4) {
+        $('.hide4').hide();
+        clicked4 = true;
+    }
+    return
+});
