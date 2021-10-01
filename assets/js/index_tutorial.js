2dbd667871941b24f919b14e73d36e3d8f4e7d2a
JoshuaT <jttsat170@gmail.com>
2021-10-01 14:04:49 -0400

    local storage and score added to quiz

--- assets/js/index_tutorial.js
+++ assets/js/index_tutorial.js
@@ -6,11 +6,12 @@ var ansButtons = $(".list-group")
 var quesHolder= $(".question")
 var answers = $(".answer")
 var indicator= $("#indicator")
+var score = 0;
 
 //Quiz object
 var quiz = {
     questions: ['Where do you obtain your APIKey','Where in the URL do you start implementing your changes','Why do you need to return as a .json','what is .then'],
-    answers: ['Off the website that you are obtaining data on', 'from my github','from your instructor', 'from Google','after.com','the very end','It means next','after https://','to make it pretty','To manipulate the data into an array','To display changes to the html page','to style it correctly',
+    answers: ['Off the website that you are obtaining data on', 'from my github','from your instructor', 'from Google','after.com','the very end','after the ?','after https://','to make it pretty','To manipulate the data into an array','To display changes to the html page','to style it correctly',
     'It is a promise','it is a object','it is a function','it is a variable'],
     correctAns:['Off the website that you are obtaining data on','after the ?','To manipulate the data into an array','It is a promise'],
 }
@@ -26,7 +27,9 @@ startButton.on("click", function() {
     $(ansButtons).css("display","initial");
 
     //show quiz content
-    quizRunner();   
+    quizRunner(); 
+    
+    
 });
 
 //function to change questions
@@ -58,7 +61,10 @@ var quizRunner= function () {
 //function for end of quiz
 var quizOver = function () {
 
-    $(quesHolder).text("Quiz Over! Press Start Button to go through questions again.")
+    $(quesHolder).text("Quiz Over! Press Start Button to go through questions again. Your score was " + score)
+
+    localStorage.setItem(".question", score)
+
     
     //remove ans button
     $(ansButtons).css("display","none");
@@ -71,6 +77,7 @@ var quizOver = function () {
     //reset variables
     x = 0;
     i = 0;
+    score = 0;
 
     //reset indicator buttons
     $(indicator).css("display", "none")
@@ -87,12 +94,19 @@ ansButtons.on("click",function(event) {
     if (quiz.correctAns[(i-1)] ==ans) {
         $(indicator).css("display", "initial")
         $(indicator).text("Correct")
+        score++;
+       
+        
         
     }
     else {
         $(indicator).css("display", "initial")
         $(indicator).text("Wrong")
     }
+
+    
+
+    
 
     //change question
     quizRunner();
